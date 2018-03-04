af6682f499eb2a1f6a1afbbbd2f79ef3c31d7839
Gavin <Mgavin@ensignfs.com>
2018-03-03 21:13:17 -0800

    Debugging viralnews and exploring coverage vs sentiment

--- viralnews/data_tools.py
+++ viralnews/data_tools.py
@@ -3,13 +3,13 @@ import pandas as pd
 import dask
 import dask.dataframe as dd
 
-def add_sent_max(df,normalize=False,drop_columns=False,normalize=False):
+def add_sent_max(df,normalize=False,drop_columns=False):
 
 	#This function is to select the most volatile sentiment from either the Title or Headline columns
 
     SentimentMax = []
 
-    for index, row in news_final.iterrows():
+    for index, row in df.iterrows():
         max_sentiment = row['SentimentTitle'] if abs(row['SentimentTitle']) > abs(row['SentimentHeadline']) else row['SentimentHeadline']
         SentimentMax.append(max_sentiment)
     
@@ -28,20 +28,28 @@ def trendify_mean(df,normalize=False,periods=7,drop_all=False,drop_alpha=False,d
 	#add trend columns to prepare for visualization
 	
 	assert isinstance(df,pd.DataFrame), "Please use a dataframe!"
-	assert isinstance(df.index,pd.DateTimeIndex), "Make sure to use time series!"
+	assert isinstance(df.index,pd.DatetimeIndex), "Make sure to use time series!"
 	
 	baseline = df.head(1).index
+
+	periods = int(periods)
 	
 	if drop_all:
 		drop_alpha, drop_nums = True, True
 		
 	numeric_columns = list(df.select_dtypes(include=['int64','float64']).columns)
-	alpha_columns = list(df.select_dtypes(include=['int64','float64']).columns)
+	alpha_columns = list(df.select_dtypes(exclude=['int64','float64']).columns)
 	
 	for c in numeric_columns:
-		df['trend_{}'.format(c)] = df[c].rolling(int(periods)).mean()
+		trend_c = 'trend_{}'.format(c)
+
+		df[trend_c] = df[c].rolling(periods).mean()
+
 		if normalize:
-			df['trend_{}'.format(c)] = df['trend_{}'.format(c)].div.loc[baseline,c].sub(1).mul(100)
+			norm_c = 'norm_'+trend_c
+
+			df[norm_c] = df[trend_c].div(df[trend_c].values[periods-1]).sub(1).mul(100)
+			df = df.drop(trend_c,axis=1)
 	
 	if drop_alpha:
 		df = df.drop(alpha_columns,index=1)
@@ -56,20 +64,28 @@ def trendify_median(df,normalize=False,periods=7,drop_alpha=False,drop_nums=Fals
 	#add trend columns to prepare for visualization
 	
 	assert isinstance(df,pd.DataFrame), "Please use a dataframe!"
-	assert isinstance(df.index,pd.DateTimeIndex), "Make sure to use time series!"
+	assert isinstance(df.index,pd.DatetimeIndex), "Make sure to use time series!"
 	
 	baseline = df.head(1).index
+
+	periods = int(periods)
 	
 	if drop_all:
 		drop_alpha, drop_nums = True, True
 		
 	numeric_columns = list(df.select_dtypes(include=['int64','float64']).columns)
-	alpha_columns = list(df.select_dtypes(include=['int64','float64']).columns)
+	alpha_columns = list(df.select_dtypes(exclude=['int64','float64']).columns)
 	
 	for c in numeric_columns:
-		df['trend_{}'.format(c)] = df[c].rolling(int(periods)).median()
+		trend_c = 'trend_{}'.format(c)
+
+		df[trend_c] = df[c].rolling(periods).median()
+
 		if normalize:
-			df['trend_{}'.format(c)] = df['trend_{}'.format(c)].div.loc[baseline,c].sub(1).mul(100)
+			norm_c = 'norm_'+trend_c
+
+			df[norm_c] = df[trend_c].div(df[trend_c].values[periods-1]).sub(1).mul(100)
+			df = df.drop(trend_c,axis=1)
 	
 	if drop_alpha:
 		df = df.drop(alpha_columns,index=1)
@@ -83,20 +99,28 @@ def trendify_std(df,normalize=False,periods=7,drop_alpha=False,drop_nums=False):
 	#add trend columns to prepare for visualization
 	
 	assert isinstance(df,pd.DataFrame), "Please use a dataframe!"
-	assert isinstance(df.index,pd.DateTimeIndex), "Make sure to use time series!"
+	assert isinstance(df.index,pd.DatetimeIndex), "Make sure to use time series!"
 	
 	baseline = df.head(1).index
+
+	periods = int(periods)
 	
 	if drop_all:
 		drop_alpha, drop_nums = True, True
 		
 	numeric_columns = list(df.select_dtypes(include=['int64','float64']).columns)
-	alpha_columns = list(df.select_dtypes(include=['int64','float64']).columns)
+	alpha_columns = list(df.select_dtypes(exclude=['int64','float64']).columns)
 	
 	for c in numeric_columns:
-		df['trend_{}'.format(c)] = df[c].rolling(int(periods)).std()
+		trend_c = 'trend_{}'.format(c)
+
+		df[trend_c] = df[c].rolling(periods).std()
+
 		if normalize:
-			df['trend_{}'.format(c)] = df['trend_{}'.format(c)].div.loc[baseline,c].sub(1).mul(100)
+			norm_c = 'norm_'+trend_c
+
+			df[norm_c] = df[trend_c].div(df[trend_c].values[periods-1]).sub(1).mul(100)
+			df = df.drop(trend_c,axis=1)
 	
 	if drop_alpha:
 		df = df.drop(alpha_columns,index=1)
